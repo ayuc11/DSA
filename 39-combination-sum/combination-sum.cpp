ba3b6bba76b8abc795334bf195ccfefc8a84adaa
AYUSH KUMAR SINGH <223069282+ayuc11@users.noreply.github.com>
2026-09-25 09:52:27 +0530

    Time: 3 ms (50.84%) | Memory: 14.3 MB (38.28%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+
+    void solve(vector<int>& candidates, int target, int start,
+               vector<int>& current) {
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); i++) {
+            if (candidates[i] > target)
+                break;
+
+            current.push_back(candidates[i]);
+
+            solve(candidates, target - candidates[i], i, current);
+
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+
+        vector<int> current;
+        solve(candidates, target, 0, current);
+
+        return result;
+    }
+};
